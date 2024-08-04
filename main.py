773818cccc4c01a0536b97fa03b1d915af11a5ac
001AM <sohampanchal1469@gmail.com>
2024-08-04 23:02:02 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,67 @@
+import aiohttp
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN: Final = '7293601483:AAGCgIlbsyshfxSgS87k09HkYfOzzu88wEQ'
+BOT_USERNAME: Final = 'Meme1468Bot'
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Hello!')
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Help')
+
+async def meme_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://meme-api.com/gimme') as response:
+            if response.status == 200:
+                data = await response.json()
+                meme_url = data.get('url')
+                if meme_url:
+                    await update.message.reply_photo(meme_url)
+                else:
+                    await update.message.reply_text('Could not fetch a meme, please try again later.')
+            else:
+                await update.message.reply_text('Could not fetch a meme, please try again later.')
+
+def handle_response(text):
+    processed = text.lower()
+    if 'hello' in processed: 
+        return 'Hey there!'
+    if 'meme' in processed: 
+        return 'meme'
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type = update.message.chat.type
+    text = update.message.text
+    print(f'User({update.message.chat.id}) in {message_type}: "{text}"')
+
+    if message_type == 'group':
+        if BOT_USERNAME in text:
+            new_text = text.replace(BOT_USERNAME, '')
+            response = handle_response(new_text)
+        else:
+            return 
+    else:
+        response = handle_response(text)
+    if response == 'meme':
+        # Call meme_command and let it handle the response
+        await meme_command(update, context)
+    elif response:
+        await update.message.reply_text(response)
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update {update} caused error {context.error}')
+
+if __name__ == '__main__':
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('help', help_command))
+    app.add_handler(CommandHandler('meme', meme_command))
+
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    app.add_error_handler(error)
+
+    app.run_polling(poll_interval=3)
